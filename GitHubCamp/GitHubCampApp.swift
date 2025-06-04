453ddea150479317e7523d8576d903f26bb10515
Kiara Eunice Abamonga <kiara.abamonga@gmail.com>
2025-06-04 13:02:47 -0500

    Initial Commit

---
+++ GitHubCamp/GitHubCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubCampApp.swift
+//  GitHubCamp
+//
+//  Created by Kiara on 6/4/25.
+//
+
+import SwiftUI
+
+@main
+struct GitHubCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
