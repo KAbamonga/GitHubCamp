8e1ce57fb172cdc5cc40841f882ebf48fe4cb2ce
Kiara Eunice Abamonga <kiara.abamonga@gmail.com>
2025-06-05 16:39:42 -0500

    made a main-2 branch

--- GitHubCamp/ContentView.swift
+++ GitHubCamp/ContentView.swift
@@ -14,8 +14,8 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
-            Text("Testing")
-            Text("Second Change")
+            Text("Testing thsis again")
+            Text("This keeps glitching")
         }
         .padding()
     }
